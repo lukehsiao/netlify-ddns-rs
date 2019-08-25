2957502f6547ebc37a3cc5a534e81cc3b1a7e514
Luke Hsiao <lwhsiao@stanford.edu>
2019-08-25 11:51:09 -0400

    test(lib): add mockito http mocks for get_external_ip
    This allows us to test the function offline with consistent results from
    any machine.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ use std::io::Read;
 
 use failure::Error;
 
+#[cfg(test)]
+use mockito;
+
 #[derive(Debug)]
 pub struct Args {
     pub domain: String,
@@ -21,6 +24,12 @@ pub enum IpType {
 
 // Get the host machine's external IP address
 fn get_external_ip(ip_type: IpType) -> Result<String, Error> {
+    #[cfg(test)]
+    let mut res = match ip_type {
+        IpType::IPV4 => reqwest::get(&mockito::server_url())?,
+        IpType::IPV6 => reqwest::get(&mockito::server_url())?,
+    };
+    #[cfg(not(test))]
     let mut res = match ip_type {
         IpType::IPV4 => reqwest::get("https://v4.ident.me/")?,
         IpType::IPV6 => reqwest::get("https://v6.ident.me/")?,
@@ -38,11 +47,23 @@ pub fn run(args: Args) -> Result<(), Error> {
 #[cfg(test)]
 mod test {
     use super::*;
+    use mockito::mock;
 
     #[test]
     fn test_get_external_ip() {
+        let _m = mock("GET", "/")
+            .with_status(200)
+            .with_header("content-type", "text/plain")
+            .with_body("104.132.34.103")
+            .create();
         let ip = get_external_ip(IpType::IPV4).unwrap();
         assert_eq!("104.132.34.103", ip.as_str());
+
+        let _m = mock("GET", "/")
+            .with_status(200)
+            .with_header("content-type", "text/plain")
+            .with_body("2620:0:1003:fd00:95e9:369a:53cd:f035")
+            .create();
 
         let ip = get_external_ip(IpType::IPV6).unwrap();
         assert_eq!("2620:0:1003:fd00:95e9:369a:53cd:f035", ip.as_str());
