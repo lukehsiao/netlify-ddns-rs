b429ae63a0bb9f3ef4886958127e855eaa8e96f6
Luke Hsiao <lwhsiao@stanford.edu>
2020-02-04 17:22:45 -0800

    test(netlify): add tests for all netlify functions

--- src/netlify.rs
+++ src/netlify.rs
@@ -20,7 +20,7 @@ pub fn get_dns_records(domain: &str, token: &str) -> Result<Vec<DNSRecord>, Erro
     );
     #[cfg(test)]
     let url = {
-        let _ = (domain, token); // supress unused variable warning in test
+        let _ = domain; // supress unused variable warning in test
         mockito::server_url()
     };
 
@@ -40,11 +40,17 @@ pub fn get_dns_records(domain: &str, token: &str) -> Result<Vec<DNSRecord>, Erro
 
 /// Delete the DNS record.
 pub fn delete_dns_record(domain: &str, token: &str, record: DNSRecord) -> Result<(), Error> {
+    #[cfg(not(test))]
     let url = format!(
         "https://api.netlify.com/api/v1/dns_zones/{}/dns_records/{}",
         domain.replace(".", "_"),
         record.id.expect("Record did not have an ID."),
     );
+    #[cfg(test)]
+    let url = {
+        let _ = (domain, record); // supress unused variable warning in test
+        mockito::server_url()
+    };
 
     let response = ureq::delete(&url).query("access_token", token).call();
     match response.status() {
@@ -59,10 +65,16 @@ pub fn delete_dns_record(domain: &str, token: &str, record: DNSRecord) -> Result
 
 /// Add a dns record to the domain.
 pub fn add_dns_record(domain: &str, token: &str, record: DNSRecord) -> Result<DNSRecord, Error> {
+    #[cfg(not(test))]
     let url = format!(
         "https://api.netlify.com/api/v1/dns_zones/{}/dns_records",
         domain.replace(".", "_"),
     );
+    #[cfg(test)]
+    let url = {
+        let _ = domain; // supress unused variable warning in test
+        mockito::server_url()
+    };
 
     let mut req = ureq::post(&url)
         .query("access_token", token)
@@ -87,7 +99,7 @@ mod test {
 
     #[test]
     fn test_get_dns_records() {
-        let body = "[{\"hostname\":\"www.example.com\",\"type\":\"NETLIFY\",\"ttl\":3600,\"priority\":null,\"weight\":null,\"port\":null,\"flag\":null,\"tag\":null,\"id\":\"5c3c343c50ab38c5d4b73003\",\"site_id\":\"d6e3d4f7-c8a5-44f3-90ab-2a4aa63ff52b\",\"dns_zone_id\":\"5c3c343b50ab38c5d4b73001\",\"errors\":[],\"managed\":true,\"value\":\"example.netlify.com\"},{\"hostname\":\"example.com\",\"type\":\"NETLIFY\",\"ttl\":3600,\"priority\":null,\"weight\":null,\"port\":null,\"flag\":null,\"tag\":null,\"id\":\"5c3c343d50ab38c5d4b73005\",\"site_id\":\"d6e3d4f7-c8a5-44f3-90ab-2a4aa63ff52b\",\"dns_zone_id\":\"5c3c343b50ab38c5d4b73001\",\"errors\":[],\"managed\":true,\"value\":\"example.netlify.com\"},{\"hostname\":\"www.example.com\",\"type\":\"NETLIFYv6\",\"ttl\":3600,\"priority\":null,\"weight\":null,\"port\":null,\"flag\":null,\"tag\":null,\"id\":\"5c3c3e41ccd232f0f0298fb9\",\"site_id\":\"d6e3d4f7-c8a5-44f3-90ab-2a4aa63ff52b\",\"dns_zone_id\":\"5c3c343b50ab38c5d4b73001\",\"errors\":[],\"managed\":true,\"value\":\"example.netlify.com\"},{\"hostname\":\"example.com\",\"type\":\"NETLIFYv6\",\"ttl\":3600,\"priority\":null,\"weight\":null,\"port\":null,\"flag\":null,\"tag\":null,\"id\":\"5c3c3e42ccd232f0f0298fbb\",\"site_id\":\"d6e3d4f7-c8a5-44f3-90ab-2a4aa63ff52b\",\"dns_zone_id\":\"5c3c343b50ab38c5d4b73001\",\"errors\":[],\"managed\":true,\"value\":\"example.netlify.com\"}]";
+        let body = "[{\"hostname\":\"www.example.com\",\"type\":\"NETLIFY\",\"ttl\":3600,\"priority\":null,\"weight\":null,\"port\":null,\"flag\":null,\"tag\":null,\"id\":\"\",\"site_id\":\"\",\"dns_zone_id\":\"\",\"errors\":[],\"managed\":true,\"value\":\"example.netlify.com\"},{\"hostname\":\"example.com\",\"type\":\"NETLIFY\",\"ttl\":3600,\"priority\":null,\"weight\":null,\"port\":null,\"flag\":null,\"tag\":null,\"id\":\"\",\"site_id\":\"\",\"dns_zone_id\":\"\",\"errors\":[],\"managed\":true,\"value\":\"example.netlify.com\"},{\"hostname\":\"www.example.com\",\"type\":\"NETLIFYv6\",\"ttl\":3600,\"priority\":null,\"weight\":null,\"port\":null,\"flag\":null,\"tag\":null,\"id\":\"\",\"site_id\":\"\",\"dns_zone_id\":\"\",\"errors\":[],\"managed\":true,\"value\":\"example.netlify.com\"},{\"hostname\":\"example.com\",\"type\":\"NETLIFYv6\",\"ttl\":3600,\"priority\":null,\"weight\":null,\"port\":null,\"flag\":null,\"tag\":null,\"id\":\"\",\"site_id\":\"\",\"dns_zone_id\":\"\",\"errors\":[],\"managed\":true,\"value\":\"example.netlify.com\"}]";
 
         let _m = mock("GET", "/")
             .match_query(Matcher::Regex("access_token.+$".into()))
@@ -99,5 +111,76 @@ mod test {
 
         let dns_records = get_dns_records("example.com", "token").unwrap();
         assert_eq!(dns_records.len(), 4);
+    }
+
+    #[test]
+    fn test_delete_dns_records() {
+        let _m = mock("DELETE", "/")
+            .match_query(Matcher::Regex("access_token.+$".into()))
+            .with_status(200)
+            .create();
+
+        assert!(delete_dns_record(
+            "example.com",
+            "token",
+            DNSRecord {
+                hostname: String::from(""),
+                dns_type: String::from(""),
+                ttl: None,
+                id: Some(String::from("example")),
+                value: String::from("example"),
+            }
+        )
+        .is_ok());
+
+        let _m = mock("DELETE", "/")
+            .match_query(Matcher::Regex("access_token.+$".into()))
+            .with_status(404)
+            .create();
+
+        assert!(delete_dns_record(
+            "example.com",
+            "token",
+            DNSRecord {
+                hostname: String::from(""),
+                dns_type: String::from(""),
+                ttl: None,
+                id: Some(String::from("example")),
+                value: String::from("example"),
+            }
+        )
+        .is_err());
+    }
+
+    #[test]
+    fn test_add_dns_records() {
+        let body = "{\"hostname\":\"test.example.com\",\"type\":\"A\",\"ttl\":3600,\"priority\":null,\"weight\":null,\"port\":null,\"flag\":null,\"tag\":null,\"id\":\"\",\"site_id\":null,\"dns_zone_id\":\"\",\"errors\":[],\"managed\":false,\"value\":\"192.0.0.1\"}";
+
+        let _m = mock("POST", "/")
+            .match_query(Matcher::Regex("access_token.+$".into()))
+            .match_header("Content-Type", "application/json")
+            .with_status(201)
+            .with_header("content-type", "application/json; charset=utf-8")
+            .with_header("content-length", &body.len().to_string())
+            .with_body(&body)
+            .create();
+
+        let resp = add_dns_record(
+            "example.com",
+            "token",
+            DNSRecord {
+                hostname: String::from(""),
+                dns_type: String::from(""),
+                ttl: None,
+                id: Some(String::from("")),
+                value: String::from(""),
+            },
+        )
+        .unwrap();
+        assert_eq!(resp.hostname, String::from("test.example.com"));
+        assert_eq!(resp.dns_type, String::from("A"));
+        assert_eq!(resp.ttl, Some(3600));
+        assert_eq!(resp.value, String::from("192.0.0.1"));
+        assert_eq!(resp.id, Some(String::from("")));
     }
 }
